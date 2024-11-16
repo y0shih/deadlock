5688db0498a8091ffabea924215a7fdcb15dc5e0
Mona Lisa <trongh1337@gmail.com>
2024-11-16 13:28:46 +0700

    EE

--- deadlock/Offsets.cs
+++ deadlock/Offsets.cs
@@ -8,10 +8,10 @@ namespace deadlock
 {
     internal static class Offsets
     {       
-        internal static IntPtr dwEntityList = 0x2092e60;        
-        internal static IntPtr ViewMatrix = 0x2276460;  
-        internal static IntPtr LocalPlayerController = 0x2265360;
-        internal static IntPtr CCitadelCameraManager = 0x20b6640;
+        internal static IntPtr dwEntityList = 0x2092e40;        
+        internal static IntPtr ViewMatrix = 0x2276440;  
+        internal static IntPtr LocalPlayerController = 0x2265340;
+        internal static IntPtr CCitadelCameraManager = 0x20b6620;
 
         internal static IntPtr m_boneArray = 0x80;
         //C_CitadelBaseAbility
